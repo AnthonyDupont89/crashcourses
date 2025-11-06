729fa99053df32c29f7d4afa491b888bfae0168f
Anthony Dupont <anthony.dupont89@gmail.com>
2025-11-06 20:14:26 +0100

    6.05

--- 6. Practice time - part 3/05. Generate sentence/index.js
+++ 6. Practice time - part 3/05. Generate sentence/index.js
@@ -9,5 +9,63 @@
 
 // Use both a for loop and a template string to solve the challenge
 function generateSentence(desc, arr) {
-    
+  let baseString = `The ${arr.length} ${desc} are `
+  const lastIndex = arr.length-1
+  for(let i = 0; i < arr.length; i++) {
+    if(i < lastIndex) {
+      baseString += `${arr[i]}, `
+    } else {
+      baseString += arr[i]
+    }
+  }
+  return "Correction: " + baseString
 }
+
+const sentence = (generateSentence("Highest mountains", ["Mount Everest", "K2"]))
+console.log(sentence);
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+let countryDesc = "largest countries"
+let countryArr = ["China", "India", "USA"]
+
+function generateSentence1(desc, arr) {
+  let itemsArr = ""
+  for(let i = 0; i < arr.length; i++) {
+    if(i < arr.length-2) {
+      itemsArr += `${arr[i]}, `
+    } else if (i < arr.length-1){
+      itemsArr += `${arr[i]} and `
+    } else {
+      itemsArr += arr[i]
+    }
+  }
+  return `Attempt 1: The ${arr.length} ${desc} are ${itemsArr}`
+}
+
+console.log(generateSentence1(countryDesc, countryArr));
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+let largestCountries = [
+  {desc: "largest countries"},
+  {arr: ["China", "India", "USA"]}
+]
+
+let bestFruits = [
+  {desc:"best fruits"},
+  {arr: ["apple", "banana"]}
+]
+
+function generateSentence0(obj) {
+  let desc = obj[0].desc
+  let arr = obj[1].arr
+  let arrLength = obj[1].arr.length
+  return `Attempt 0: The ${arrLength} ${desc} are ${arr}`
+}
+
+console.log(generateSentence0(bestFruits));
