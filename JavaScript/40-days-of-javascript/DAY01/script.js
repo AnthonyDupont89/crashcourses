044696199072dea23e908061fec8ad5808af04f2
Anthony Dupont <anthony.dupont89@gmail.com>
2025-11-17 17:23:31 +0100

    day01

---
+++ JavaScript/40-days-of-javascript/DAY01/script.js
@@ -0,0 +1,5 @@
+// console.log("Hello from Node.sj");
+
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of Javascript!");
+document.write("Check the browser console for a message!")
